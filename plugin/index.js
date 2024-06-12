51576453aaf1617eb7fcd6573a894434ac9b199f
karthik2804 <karthik.ganeshram@fermyon.com>
2024-06-12 12:45:37 +0200

    small cleanup and prepare for alpha release
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- plugin/index.js
+++ plugin/index.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 module.exports = class WasiExtPlugin {
     constructor() {
         this.fallbacks = {
@@ -8,20 +9,16 @@ module.exports = class WasiExtPlugin {
     }
 
     apply(compiler) {
-        compiler.hooks.afterPlugins.tap('WasiExtPlugin', (compiler) => {
-            const webpack = compiler.webpack || require('webpack');
-
-            compiler.options.resolve.fallback = {
-                ...this.fallbacks,
-                ...compiler.options.resolve.fallback,
-            };
-
-            // Adding ProvidePlugin configuration
-            compiler.options.plugins.push(
-                new webpack.ProvidePlugin({
-                    Buffer: ['buffer', 'Buffer'],
-                })
-            );
+        const providePlugin = new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+            process: [require.resolve('../lib/process'), 'default']
         });
+
+        compiler.options.resolve.fallback = {
+            ...compiler.options.resolve.fallback,
+            ...this.fallbacks,
+        };
+
+        compiler.options.plugins.push(providePlugin);
     }
 };
